4d818c1bee0db0d6cdde59d765c4d8fb3878f5a8
Derrick <dbozich1@gmail.com>
2018-05-23 16:14:10 -0600

    added mobile nav bar functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let navItems = document.getElementsByClassName('nav');
+let active = false;
+
+
+function myFunction(){
+  // for (let i = 0; i < navItems.length; i++){
+  //   console.log(navItems);
+  // }
+
+  if (active){
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].className = navItems[i].className.replace(" responsive", "");
+     }
+     active = false;
+
+  } else {
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].className += " responsive";
+     }
+     active = true;
+
+  }
+
+
+
+
+console.log(navItems);
+
+
+
+
+
+}
